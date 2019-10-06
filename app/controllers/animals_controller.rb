cb458c60405640c01684c302ac126d30d63a49d8
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-06 15:01:04 -0700

    added animals#show action

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -2,4 +2,9 @@ class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
   end
+  
+  def show
+    animal_id = params[:id]
+    @animal = Animal.find_by(id: animal_id)
+  end
 end
