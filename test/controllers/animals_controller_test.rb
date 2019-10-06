241487390fc24e7cf3413219f4a760353503b2fe
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-10-06 15:09:21 -0700

    added tests for animals#show

--- test/controllers/animals_controller_test.rb
+++ test/controllers/animals_controller_test.rb
@@ -1,14 +1,27 @@
 require "test_helper"
 
 describe AnimalsController do
+  let(:animal) {
+    Animal.create(name: "Ivy", species: "Wildcat", age: 7)
+  }
   
   describe "index" do
-    
     it "gets the index path" do
       get animals_path
       must_respond_with :success
     end
+  end
+  
+  describe "show" do
+    it "gets the show path" do
+      get animal_path(animal.id)
+      must_respond_with :success
+    end
     
+    it "redirects to index for nonexistent animals" do
+      get animal_path(-1)
+      must_redirect_to animals_path
+    end
   end
   
 end
